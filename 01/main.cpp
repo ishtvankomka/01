ff87ec11ee5aab27fa9f1ea1e1b6f801e86e440e
Ishtvan <Komonyi.istvan@gmail.com>
2021-11-15 21:24:51 +0200

    first master commit

---
+++ 01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  01
+//
+//  Created by mac on 11/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
